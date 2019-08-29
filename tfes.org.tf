a91cd47830581294837fe7b55a069eb05063e12f
pardisfla <parsifal@tfes.org>
2019-08-30 01:19:49 +0200

    Add mail-related records

--- tfes.org.tf
+++ tfes.org.tf
@@ -8,6 +8,35 @@ output "tfes_org_ns" {
 	value       = cloudflare_zone.tfes_org.name_servers
 }
 
+resource "cloudflare_record" "tfes_org_mx" {
+	domain = cloudflare_zone.tfes_org.zone
+	name   = "tfes.org"
+	type   = "MX"
+	ttl    = 900
+	value  = "palpatine.tfes.org"
+}
+resource "cloudflare_record" "tfes_org_spf" {
+	domain = cloudflare_zone.tfes_org.zone
+	name   = "tfes.org"
+	type   = "SPF"
+	ttl    = 900
+	value  = "v=spf1 ip4:173.230.144.109/32 ip6:2600:3c01::f03c:91ff:fe93:a60c/128 -all"
+}
+resource "cloudflare_record" "tfes_org_txt_spf" {
+	domain = cloudflare_zone.tfes_org.zone
+	name   = "tfes.org"
+	type   = "TXT"
+	ttl    = 900
+	value  = "v=spf1 ip4:173.230.144.109/32 ip6:2600:3c01::f03c:91ff:fe93:a60c/128 -all"
+}
+resource "cloudflare_record" "x201709_domainkey_tfes_org_txt_dkim" {
+	domain = cloudflare_zone.tfes_org.zone
+	name   = "201709._domainkey.tfes.org"
+	type   = "TXT"
+	ttl    = 900
+	value  = "v=DKIM1; h=sha256; k=rsa; s=email; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2L4uT5EP2cwQm0uyAwAUfqZ5KmATPo/5EXbhV8s2wgHh2GfH1ZR/wEzvYW3jejHQeWkzyJuyyxQ/jQcQ7dOFN+Xb5LmI5+aNvZRInkoRB9ttsQ4jvovZF12AHwdVZMDnWIBzedwQ1jP0ZSXr+IhLK1bc1+sLX3XWm8RuC1avPh6lsAC4XuJ/1jfGulUYi54mJuH6RTbOXYmEkAP1M7TLTf9MdT72jHTaVk94QVv0Qud+ORP0FqKzXwRIFURCUVukOCtWr2clCZzWpHPeqjyfWNKRRi2+t2/tGgE9YkbFZh7M405iU1pQ+KoHxvd+i7fo57E2+uCdbfAUy2APN05u8QIDAQAB"
+}
+
 module "tfes_org" {
 	source  = "./palpatine"
 	domain  = cloudflare_zone.tfes_org.zone
